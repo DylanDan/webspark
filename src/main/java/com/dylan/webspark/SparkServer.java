cc8e152ae36f3be0cc3b7d586c0ccc231338e52e
dylandan <414830134@qq.com>
2016-04-29 00:52:48 +0800

    add port setting

--- src/main/java/com/dylan/webspark/SparkServer.java
+++ src/main/java/com/dylan/webspark/SparkServer.java
@@ -5,7 +5,20 @@ import spark.Spark;
 public class SparkServer {
 	
 	    public static void main(String[] args) {
-	    	Spark.secure("D:/test.jks", "12345678", null, null);
+	    	//Spark.secure("D:/test.jks", "12345678", null, null);
+	    	if(args.length==0){
+	    		System.out.println("parameter can not be null");
+	    		System.exit(-1);
+	    	}
+	    	
+	    	int port = Integer.valueOf(args[0]);
+	    	
+	    	if(port< 1024){
+	    		System.out.println("the port should bigger than 1024");
+	    		System.exit(-1);
+	    	}
+	    	
+	    	Spark.port(port);
 	        Spark.get("/hello", (req, res) -> "Hello World");
 	    }
 }
